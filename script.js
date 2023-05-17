6e319f47d628cc8c98087fbc47f65c36e2285a28
AshenIwantha <98282709+AshenIwantha@users.noreply.github.com>
2023-05-17 11:39:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function fn(customPromise) {
+	// return a new Promsie which resolves to + 1 of the resolved promise value
+    return customPromise.then(result => {
+        return result + 1;
+    })
+}
+
+module.exports = fn
+
+console.log(fn())
